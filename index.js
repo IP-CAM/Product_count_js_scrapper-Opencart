96c932a5640fd2510dbcb77b3344be77803d8479
Anant Negi <54684361+codeeshop-oc@users.noreply.github.com>
2020-11-28 11:17:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 let atags = document.querySelectorAll('.extension-preview a')
 let total_count = 0  
-myfunc()
+getURLS()
+
+async function getURLS() {
+  if(!atags.length) {  
+    await $.get('https://www.opencart.com/index.php?route=marketplace/extension&filter_member=codeeshop', function(html, status){     
+       atags = $(html).find('.extension-preview a')
+    })
+  }
+  myfunc()
+}
 
 async function myfunc() {
   let data = []
   for(i=0; i< atags.length; i++) {
     let url = atags[i].getAttribute('href')
-    await $.get(url, function(html, status){    
+    await $.get(url, function(html, status){
        let tdata = {
           'count': $(html).find('#sales strong').text(),
           'name': $(html).find('.container h3').text(),

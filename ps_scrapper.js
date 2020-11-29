27c6fd66864aa6a7dd0952ebe25c53a4cf959883
Anant Negi <54684361+codeeshop-oc@users.noreply.github.com>
2020-11-29 13:42:56 +0530

    Update ps_scrapper.js

--- ps_scrapper.js
+++ ps_scrapper.js
@@ -9,10 +9,10 @@ setTimeout(function () {
 	let total_count = 0  
 	let datas = []
 	// const pagesToGet = [1,2,3,4,5,6,7]
-	const pagesToGet = [1]
+	const pagesToGet = [5]
 
 	async function getURLS(i) {
-	  await $.get('https://addons.prestashop.com/en/2-modules-prestashop?nb_item=24&page=' + i, function(html, status){
+	  await $.get('https://addons.prestashop.com/en/2-modules-prestashop?nb_item=96&page=' + i, function(html, status){
 	     atags.push($(html).find('#products-list > div a'))
 	  })
 	}
@@ -25,10 +25,13 @@ setTimeout(function () {
 		  	console.log(url)
 		    await $.get(url, function(html, status){
 		    	let nchild = 7
-		    	if($(html).find('[itemprop="offers"] .free-text').text() == 'Free') {
+		    	let str = $(html).find('.specs-table .spec-line:nth-child(7) .primary-text.small-text.text').text()
+		    	if($(html).find('[itemprop="offers"] .free-text').text() == 'Free' || str.indexOf('Number') != 0) {
 		    		nchild = 6
 		    	}
-		        let tdata = {
+		    	console.log(str, nchild)
+		    	
+		    	let tdata = {
 		          'sale_count': getcount($(html).find('.specs-table .spec-line:nth-child(' + nchild + ') .spec-data').text().trim()),
 		          'price': $(html).find('[itemprop="price"]').attr('content'),
 		          'name': trimspace($(html).find('#product_title').text().trim()),
@@ -65,6 +68,7 @@ setTimeout(function () {
 	}
 
 	function getcount(str) {
+		str = str.replace(/en+|fr+|es+|it+|de+/g, '', str)
 		str = str.replace(/\s\s+/g, ' ', str)
 		str = str.replace(/  +/g, ' ', str)
 		return str.replace(/,/g, '', str)

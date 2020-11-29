5beaef3ea95fd046b3f49cb1326732470180f280
Anant Negi <54684361+codeeshop-oc@users.noreply.github.com>
2020-11-29 12:54:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
-let atags = document.querySelectorAll('.extension-preview a')
+let atags = []
 let total_count = 0  
-getURLS()
+let datas = []
+const pagesToGet = [1,2,3,4,5,6,7]
+// const pagesToGet = [1,2]
 
-async function getURLS() {
-  if(!atags.length) {  
-    await $.get('https://www.opencart.com/index.php?route=marketplace/extension&filter_member=codeeshop', function(html, status){     
-       atags = $(html).find('.extension-preview a')
-    })
-  }
-  myfunc()
+async function getURLS(i) {
+  await $.get('https://www.opencart.com/index.php?route=marketplace/extension&filter_member=iSense&page=' + i, function(html, status){
+     atags.push($(html).find('.extension-preview a'))
+  })
 }
 
 async function myfunc() {
   let data = []
-  for(i=0; i< atags.length; i++) {
-    let url = atags[i].getAttribute('href')
-    await $.get(url, function(html, status){
-       let tdata = {
-          'count': $(html).find('#sales strong').text(),
-          'name': $(html).find('.container h3').text(),
-       }
+  for(y=0; y< atags.length; y++) {
+	  for(i=0; i< atags[y].length; i++) {
+	    let url = atags[y][i].getAttribute('href')
+	  	// console.log(url)
+	    await $.get(url, function(html, status){
+	       let tdata = {
+	          'sale_count': $(html).find('#sales strong').text(),
+	          'name': $(html).find('.container h3').text(),
+	       }
 
-       console.log('checking ' + tdata.name + ' : ' + tdata.count)
-       data.push(tdata)
-       total_count += parseInt(tdata.count)
-    })
+	       datas.push(tdata)
+	       total_count += parseInt(tdata.sale_count)
+	    })
+	  }
   }
-  console.log(data, total_count);
 }
+
+const mapLoop = async _ => {
+  console.log('Start')
+
+  const promises = pagesToGet.map(async page => {
+    const numFruit = await getURLS(page)
+    return numFruit
+  })
+
+
+  const numFruits = await Promise.all(promises)
+  await myfunc()
+
+  console.table(datas)
+  console.log('total_count : ', total_count)
+  console.log('End')
+}
+
+mapLoop()

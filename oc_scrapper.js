3e24be516e0a63666da95b694f404883c10bcc58
Anant Negi <54684361+codeeshop-oc@users.noreply.github.com>
2020-12-22 17:26:43 +0530

    Update oc_scrapper.js

--- oc_scrapper.js
+++ oc_scrapper.js
@@ -13,15 +13,16 @@ setTimeout(function () {
   let atags = []
   let total_count = 0  
   let datas = []
-  var pages = 100
+  var pages = 20
   var pagesToGet = []
 
-  for(let i = 1; i < pages; i++) {
+  for(let i = 1; i <= pages; i++) {
     pagesToGet.push(i)
   }
 
   async function getURLS(i) {
     await $.get('https://www.opencart.com/index.php?route=marketplace/extension&filter_member=codeeshop&page=' + i, function(html, status){
+        $(html).find('img').remove()
        atags.push($(html).find('.extension-preview a'))
     })
   }
@@ -35,10 +36,11 @@ setTimeout(function () {
         await $.get(url, function(html, status){
           //console.log(status, 'status')
            let tdata = {
-              'sale_count': $(html).find('#sales strong').text(),
-              'name': $(html).find('.container h3').text(),
+              'sale_count': parseInt($(html).find('#sales strong').text()),
+              'name': $(html).find('.container h3:first').text(),
       	      'comment': parseInt($(html).find('#comment strong').text()),
       	      'price': $(html).find('#price .text-right').text(),   
+              'url': url,
            }
 
            datas.push(tdata)
@@ -51,7 +53,6 @@ setTimeout(function () {
 
   const mapLoop = async _ => {
     console.log('Start')
-
     const promises = pagesToGet.map(async page => {
       const numFruit = await getURLS(page)
       return numFruit
@@ -62,6 +63,7 @@ setTimeout(function () {
 
     datas.sort(GetSortOrder("sale_count"));
     console.table(datas)
+    copy(datas)
     console.log('total_count : ', total_count)
     toastr["success"]('total_count : ' + total_count)
     console.log('End')
@@ -76,8 +78,6 @@ setTimeout(function () {
         }    
         return 0;    
     }    
-  }    
-
-
+  }
   mapLoop()  
 }, 2000)
